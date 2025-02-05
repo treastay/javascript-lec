151b8bcb83c7b6ed03f3f554b0617fcfc24ef304
Tanvi <tanvigs2003@gmail.com>
2025-02-05 20:03:29 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1- variable declaration
+
+const name ="Sawant Tanvi"
+const phoneNumber = 9324248830
+
+console.log(`my name is ${name} and my phone number is ${phoneNumber}`)
+console.log(name)
+
+var myname = "Tanvi Sawant"
+console.log(myname)
+
+let myname2 = "Tanvi G. Sawant"
+console.log(myname2)
+
+//2. operators
+//Arithematic operators
+
+//Logical
+const num1 = 4
+const num2 = 4
+console.log(num1 = num2)
+
+
+var myname = "Tanvi Sawant"
+console.log(myname)
+
+let myname3 = "Tanvi G. Sawant"
+console.log(myname2)
+
+//2. operators
+//Arithematic operators
+
+// //Logical
+const num3 = "5"
+const num4 = 4
+let tanvi = num1==num2
+
+
+console.log(!tanvi) // corrected the operator to check for equality
+
+// added a comment to explain the purpose of the code
+// checking if num1 is equal to num2 and logging the result)
+
+const first = "tanvi"
+const second = null
+
+console.log(!second)
+// null and undefined are datatypes
+// they are equal to false
+
+let loginid = null;
+let password = 12345678;
+
+if (!loginid) {
+    console.log("login id is not available");
+} else {
+    console.log("login id is available");
+}
+//3-conditional statements
+//4-Loops
+//5-Functions
+
+
