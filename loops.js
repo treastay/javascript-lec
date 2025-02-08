a0c89ec71a7613cec2e6903f06c5697fb2170416
Tanvi <tanvigs2003@gmail.com>
2025-02-08 14:11:38 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,103 @@
+// for(let i=0 ; i<10 ;i=i+1){
+//console.log(i);
+// }
+
+// let i=0;
+// while(i<10){
+//     console.log(i);
+//     i=i+2;
+// }
+
+// const numbers = [2,4,6,8,10,12,14,16,18,20];
+
+// const mapping = numbers.map(div);
+// function div(i)
+// {
+//     return i/2;
+// }
+// console.log(mapping);    
+
+// const fruits = ["Apple","Orange","Banana","Peach","Grapes"]
+// fruits.splice(2,1,"SweetLime")
+// console.log(fruits);
+
+// const num = [2,4,2,4,8,8,3,0]
+// console.log(num.unshift(9,3,2,4))
+// console.log(num);
+
+// function multiply(a,b){
+//     console.log (a*b);
+// }
+
+// multiply(40,30)
+
+// const add = (a,b) => console.log(a/b)
+// add(40,30)
+
+// 5 unique array,String,num ke methods leke practise krne hai
+
+//Array flat() Method
+
+// let a1 = [1,2,3,[2,1,4,6]];
+// console.log(a1.flat(Infinity));
+
+// Array filter() method
+
+// let square = [4, 9, 16, 25, 36]
+// let a1 = square.filter((num) => num > 22)
+// console.log(a1)
+
+// Array shift() method 
+
+// let array = [true,2,5,"birds"]
+// array.shift();
+// console.log(array);
+
+//Array some() method
+
+// let square = [4, 9, 16, 25, 36]
+// let a1 = square.some((num) => num > 22)
+// console.log(a1)
+
+// Array toString() Method
+
+// let array = [1,2,3,4,5]  
+// console.log(array.toString());
+
+//String slice() Method
+// let T = "Hello World"
+// let a1 = T.slice(6, 11)
+// console.log(a1)
+
+
+//String replace() Method
+// let T = "Tanvi Sawant"
+// let a1 = T.replace("Tanvi", "Girish")
+// console.log(a1)
+
+//String padStrings() Method
+// let T = "Tanvi Sawant"
+// let a1 = T.padStart(20, "Girish")
+// console.log(a1)
+
+//String padEnd() Method
+// let T = "Tanvi Sawant"
+// let a1 = T.padEnd(20, "Girish")
+// console.log(a1)
+
+//String trimStart() Method
+// let T = "   Tanvi Sawant   "
+// let a1 = T.trimStart()
+// console.log(a1)
+
+
+
+
+
+
+
+
+
+
+
+
